d4412e142345640a38a5dce91e3806ed1489ec97
David <dmaine6711@gmail.com>
2023-12-13 16:31:15 -0500

    crush phase 1

--- app/javascript/follow_toggle.js
+++ app/javascript/follow_toggle.js
@@ -2,11 +2,25 @@ import { API, broadcast } from "./util";
 
 export default class FollowToggle {
   constructor(toggleButton) {
+    this.toggleButton = toggleButton;
     // Your code here
+    this.toggleButton.addEventListener('click', this.handleClick.bind(this))
+    this.followState = toggleButton.dataset.followState
+    console.log(toggleButton.dataset)
   }
+
+  
 
   async handleClick(event) {
     // Your code here
+    event.preventDefault();
+
+    if (this.followState === 'followed') {
+      this.unfollow();
+    } else {
+      this.follow();
+    }
+    console.log(this.followState);
   }
 
   async follow() {
--- app/javascript/util/api.js
+++ app/javascript/util/api.js
@@ -3,8 +3,20 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
+    'X-CSRF_TOKEN': csrfToken,
     ...options.headers
   };
 
   return await fetch(url, options);
+}
+
+
+export const foo = "bar";
+
+export const followUser = (id) => {
+  return customFetch( `/users/${id}/follow` , {method: 'POST'})
+}
+
+export const unfollowUser = (id) => {
+  return customFetch(`/users/${id}/follow` , {method: 'DELETE'})
 }
